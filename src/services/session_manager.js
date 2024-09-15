168ccb806d6c7e052df5e3a49aa173097c37a803
ravi-dholariya <200540107103@darshan.ac.in>
2024-09-15 23:06:51 +0530

    removed redis password

--- src/services/session_manager.js
+++ src/services/session_manager.js
@@ -4,7 +4,6 @@ import Redis from "ioredis";
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
 });
 
 export const saveSession = async (clientId, sessionData) => {
